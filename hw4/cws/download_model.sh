193d3e93e694791b2f321a421500a820b87db294
wubinary <wubinray@gmail.com>
2020-05-20 11:20:05 +0800

    download model

---
+++ hw4/cws/download_model.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+download_from_gdrive() {
+	file_id=$1
+	file_name=$2
+			    
+	# first stage to get the warning html
+	curl -c /tmp/cookies \
+	"https://drive.google.com/uc?export=download&id=$file_id" > \
+	/tmp/intermezzo.html
+
+	# second stage to extract the download link from html above
+	download_link=$(cat /tmp/intermezzo.html | \
+	grep -Po 'uc-download-link" [^>]* href="\K[^"]*' | \
+	sed 's/\&amp;/\&/g')
+	curl -L -b /tmp/cookies \
+	"https://drive.google.com$download_link" > $file_name
+}
+
+###############################################
+################   my_cws_bert.pt   ###############
+if [ ! -f "OUT_DIR/my_cws_bert.pt" ]; then
+	echo " [Info] download my_cws_bert.py model "
+	download_from_gdrive 1cux6iBOmD3FBaDFti2bbmRNPvFENrnCz OUT_DIR/my_cws_bert.pt  
+fi 
+
